c0cea5f8e3f4b5c9be60b7337f09242936e564c7
raphaeldoukhan <raphael.doukhan@live.fr>
2019-09-17 16:48:29 +0200

    add front homepage + js of tabs

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,7 +1,29 @@
 class OffersController < ApplicationController
   before_action :authenticate_buyer!
   def index
+    @liste_legumes = []
+    @liste_legumineuses =[]
+    @liste_cereales =[]
+    @liste_fruits = []
+
     @offers=Offer.all
+
+    @legumes = Product.where(prod_category: "LEGUME").ids
+    @legumineuses = Product.where(prod_category: "LEGUMINEUSE").ids
+    @cereales = Product.where(prod_category: "CEREALE").ids
+    @fruits = Product.where(prod_category: "FRUIT").ids
+
+    @offers.each do |x| 
+      if @legumes.include?(x.product_id.to_i)
+      @liste_legumes << x
+      elsif @legumineuses.include?(x.product_id.to_i)
+        @liste_legumineuses << x
+      elsif @cereales.include?(x.product_id.to_i)
+        @liste_cereales << x 
+      elsif @fruits.include?(x.product_id.to_i)
+        @liste_fruits << x  
+      end
+    end
   end
 
   def show
