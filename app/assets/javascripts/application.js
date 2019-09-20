d166d8c91f47493c02bf1c694de2e5998105be86
Julien <julien63650@gmail.com>
2019-09-20 17:35:02 +0200

    adding js for burger button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -58,3 +58,12 @@ function hideOnClick() {
   }; 
 
 
+  function showBurgerButton() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  
+  } 
